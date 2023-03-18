5cb51514c73caf1ae3f4cee28a5a3d8328ca5feb
DevilsDomain <alaraerzincan@gmail.com>
2023-03-18 17:01:14 +0100

    displays movie details on details page

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -1,11 +1,20 @@
-'use client';
 import React from 'react'
 import 'src/app/movie/[movieId]/page.css'
 
-function page({ params }) {
+export default async function page({ params }) {
+  const data = await fetch('https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json')
+  const res = await data.json()
+
+  // Find the movie with the matching ID
+  const movie = res.find((m) => m.imdb_url.slice(7) === `${params.movieId}/`)
+
+  if (!movie) {
+    return <p>Movie not found</p>
+  }
+
   return (
-    <p>Movie detail {params.movieId}</p>
+    <div>
+      <p>{movie.name}</p>
+    </div>
   )
 }
-
-export default page
