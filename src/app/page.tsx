293c5ade985e6fc269f096f5a613a96eb3270c43
DevilsDomain <alaraerzincan@gmail.com>
2023-03-18 13:44:32 +0100

    sort movies in ascending order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,18 @@ const inter = Inter({ subsets: ['latin'] })
 export default async function Home() {
   const data = await fetch('https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json')
   const res = await data.json()
-  console.log(res)
+
+  // Sort movies by rating in ascending order
+  res.sort((a, b) => a.rating - b.rating)
+
   return (
     <main>
       <div className='grid'>
-        {res.map((movie, movieIndex) => {
-          return(
-            <MoviePoster key={movieIndex} image={movie.image_url} title={movie.name} rating={movie.rating} year={movie.year}/>
-          );
-        })}
+        {res.map((movie, movieIndex) => (
+          <MoviePoster key={movieIndex} image={movie.image_url} title={movie.name} rating={movie.rating} year={movie.year} />
+        ))}
       </div>
     </main>
   )
 }
+
