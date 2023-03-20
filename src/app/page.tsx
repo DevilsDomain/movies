b0dbc725fe62a8eb47349618eb637ae93974390f
DevilsDomain <alaraerzincan@gmail.com>
2023-03-20 11:01:10 +0100

    main page ts errors fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import '../app/page.css'
 import MovieList from '@/components/MovieList/MovieList'
 
+interface Movie {
+  rating: number;
+
+}
+
 
 export default async function Home() {
   const data = await fetch('https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json')
   const res = await data.json()
 
   // Sort movies by rating in ascending order
-  res.sort((a, b) => a.rating - b.rating)
-
+  res.sort((a: Movie, b: Movie) => a.rating - b.rating);
   return (
     <main>
       <MovieList data={res} />
