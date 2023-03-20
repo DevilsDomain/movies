95afc1c969a93db5838f92aa8132ec100afd1980
DevilsDomain <alaraerzincan@gmail.com>
2023-03-20 09:14:53 +0100

    add/ remove to favorites

--- src/components/MoviePoster/MoviePoster.tsx
+++ src/components/MoviePoster/MoviePoster.tsx
@@ -1,21 +1,33 @@
 'use client';
-import React from 'react'
-import Image from 'next/image'
-import '../MoviePoster/MoviePoster.css'
+import React from 'react';
+import Image from 'next/image';
+import '../MoviePoster/MoviePoster.css';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-function MoviePoster({image, title, rating, year, movieId}) {
+function MoviePoster({ image, title, rating, year, movieId }) {
   const router = useRouter();
+  const [fave, setFave] = useState(false);
+
+  const handleFavorite = () => {
+    setFave(!fave);
+  };
+
   return (
     <div className='pos-relative'>
-        <div className='text pos-absolute'>
-            <p onClick={() => router.push(`/movie/${movieId}`)}>{title}</p>
-            <p>{rating}</p>
-            <p>{year}</p>
-        </div>
-        <Image src={image} width={200} height={400} className='image'/>
+      <div className='text pos-absolute'>
+        <p onClick={() => router.push(`/movie/${movieId}`)}>{title}</p>
+        <p>{rating}</p>
+        <p>{year}</p>
+      </div>
+      <Image src={image} width={200} height={400} className='image' />
+      {fave ? (
+        <div onClick={handleFavorite}><h4>❌</h4></div>
+      ) : (
+        <div onClick={handleFavorite}><h4>✅</h4></div>
+      )}
     </div>
-  )
+  );
 }
 
-export default MoviePoster
+export default MoviePoster;
